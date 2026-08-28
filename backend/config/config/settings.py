3e3e4a83ac18c2f89bfda219c0a488bee3b245cd
Arun k <arun200325@gmail.com>
2026-08-28 14:42:39 +0530

    Prepare backend for deployment

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -24,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1kv)vhba=z^)_@$$d203h1zkhx*k^o_)251btvh!o$m)l_p6rp'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
