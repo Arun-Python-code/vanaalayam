b8bcb19e4ba6d804f7281faffd2cda3d14f245ed
Arun k <arun200325@gmail.com>
2026-09-03 15:50:28 +0530

    Fix Render allowed hosts

--- backend/config/config/settings.py
+++ backend/config/config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ "https://vanaalayam.onrender.com",
+ALLOWED_HOSTS = [ "vanaalayam.onrender.com",
     "localhost",
     "127.0.0.1",]
 
@@ -162,3 +162,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://vanaalayam.netlify.app",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://vanaalayam.onrender.com",
+    "https://vanaalayam.netlify.app",
+]
